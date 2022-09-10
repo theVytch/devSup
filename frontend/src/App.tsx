a08e5f3339390e4b0d41628792fdd6b77b276cfe
TheVytch <edu.vytch@gmail.com>
2022-09-10 20:52:13 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
   return (
-  <h1>Ola mundo</h1>
-  )
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
