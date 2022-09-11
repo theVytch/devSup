c2cb8ffae904c89806091d482f3cae08d9bf69ef
TheVytch <edu.vytch@gmail.com>
2022-09-10 22:31:29 -0300

    API test

---
+++ backend/src/main/java/com/dev/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.dev.dsmeta.services;
+
+import com.dev.dsmeta.entities.Sale;
+import com.dev.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public List<Sale> findSales(){
+        return saleRepository.findAll();
+    }
+}
